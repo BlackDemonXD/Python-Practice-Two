025e3725d8ad048ef5141a96d09efcd67276fc78
BlackDemonXD <radidraiyan@gmail.com>
2022-04-12 13:37:05 +0600

    Created string.py and edited it to include slicing data

---
+++ stage-2/string.py
@@ -0,0 +1,7 @@
+message = "GNU/Linux's world" #Avoiding collision by using double quotes
+print(message)                #Printing message in the message variable
+print(len(message))           #Length of the variable message
+print(message[0])             #Print the 0 numbered character of the message variable
+print(message[0:5])           #Print from the 0 to the 4th (does NOT include the 5th variable) from the message varaible
+print(message[:5])            #With the same message, leaving the first parenthesis as blank before colon assumes it starts from the beginning, ie- 0
+print(message[5:])            #Again if we take the first parenthesis as 5 and the area after colon as blank, it will assume to display variables till the end
