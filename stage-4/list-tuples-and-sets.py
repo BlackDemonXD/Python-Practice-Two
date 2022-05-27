072f1d64a3a1ad43df5beeea525c3952de37143e
BlackDemonXD <radidraiyan@gmail.com>
2022-05-27 15:39:54 +0600

    Edited list-tuples-and-sets.py

---
+++ stage-4/list-tuples-and-sets.py
@@ -0,0 +1,6 @@
+#Lists
+courses=['Cringe','No ask','Any Askers','Fatherless']
+print(courses)       #Print each of the values of courses
+print(len(courses))  #Prints the values in the list 'courses'
+print(courses[0])    #Print the 0 (n'th) value of 'courses'
+print(courses[-1])   #Prints the last value of 'courses'
